a94be60e1d49e1475a39e535143e5d8a2bd0d661
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2018-05-16 22:37:07 +0200

    removed unused code

--- js/main.js
+++ js/main.js
@@ -11,11 +11,6 @@ var svg = d3.select("body").append("svg").attr("width", 700).attr("height", 400)
 svg.append("g").attr("id","salespie");
 
 gradPie.draw("salespie", randomData(), 200, 200, 160);
-
-function changeData(){
-	gradPie.transition("salespie", randomData(), 160);
-}
-
 function randomData(){
 	return salesData.map(function(d){
 		return {label:d.label, value:1000*Math.random(), color:d.color};});
